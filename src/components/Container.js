82ec134386963b461029347baf23a2800ae0751a
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-07-09 19:12:46 -0400

    added pages component

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,67 +1,39 @@
 import '../stylesheets/Container.css'
+import Pages from './Pages'
 
 function Container() {
-  function pageFlip(e) {
-    if (e.target.classList.contains("book__page-front")) {
-      e.target.parentNode.style.transition = "z-index 1.5s linear 0.5s, transform 1.5s"
-      e.target.parentNode.style.transform = "rotateY(-180deg)"
-      const index = parseInt(e.target.parentNode.style.zIndex) * -1
-      e.target.parentNode.style.zIndex = `${index}`
-    } else {
-      e.target.parentNode.style.transition = "z-index 1s, transform 1.5s"
-      e.target.parentNode.style.transform = ""
-      const index = parseInt(e.target.parentNode.style.zIndex) * -1
-      e.target.parentNode.style.zIndex = `${index}`
-    }
+  const arr = Array.from(Array(50).keys())
+  // console.log(arr)
+  
+  const pages = arr.slice(4, -(arr.length % 4))
+  // console.log(showPages)
+
+  const pageTags = []
+
+  for (let i = 0; i < pages.length; i += 8) {
+    const cur_pages = pages.slice(i, i + 8)
+    pageTags.push(<Pages key={i} items={cur_pages} zIndex={arr.length - i} page={(i + 8) / 8 + 1} />)
   }
+
+  console.log(pageTags)
 
   return (
     <div id='container'>
       <div className="cover">
         <div className="book">
           <div className="book__page book__page--1" >
-            <h1>THIS IS PAGE 1</h1>
+            <h1>THIS HAS ITEM</h1>
+            <p>{arr.slice(0, 4)}</p>
             <div className="page__number">1</div>
           </div>
 
           <div className="book__page book__page--last">
-            <h1>THIS IS PAGE last</h1>
-            <div className="page__number">last</div>
+            <h1>THIS HAS ITEM</h1>
+            <p>{arr.slice(-(arr.length % 4))}</p>
+            <div className="page__number">{Math.ceil(arr.length / 4)}</div>
           </div>
 
-          <div className="book__page book__page--innerpages" style={{zIndex: "1"}}>
-            <div className="book__page-front" onClick={pageFlip}>
-              <h1>THIS IS PAGE 6</h1>
-              <div className="page__number">6</div>
-            </div>
-            <div className="book__page-back" onClick={pageFlip}>
-              <h1>THIS IS PAGE 7</h1>
-              <div className="page__number">7</div>
-            </div>
-          </div>
-        
-          <div className="book__page book__page--innerpages" style={{zIndex: "2"}}>
-            <div className="book__page-front" onClick={pageFlip}>
-              <h1>THIS IS PAGE 4</h1>
-              <div className="page__number">4</div>
-            </div>
-            <div className="book__page-back" onClick={pageFlip}>
-              <h1>THIS IS PAGE 5</h1>
-              <div className="page__number">5</div>
-            </div>
-          </div>
-
-          <div className="book__page book__page--innerpages" style={{zIndex: "3"}}>
-            <div className="book__page-front" onClick={pageFlip}>
-              <h1>THIS IS PAGE 2</h1>
-              <div className="page__number">2</div>
-            </div>
-            <div className="book__page-back" onClick={pageFlip}>
-              <h1>THIS IS PAGE 3</h1>
-              <div className="page__number">3</div>
-            </div>
-          </div>
-
+          {pageTags}
         </div>
       </div>
     </div>
---
+++ src/components/Pages.js
@@ -0,0 +1,35 @@
+function Pages({items, zIndex, page}) {
+  const [a, b, c, d, e, f, g, h] = items
+
+  function pageFlip(e) {
+      if (e.target.classList.contains("book__page-front")) {
+        e.target.parentNode.style.transition = "z-index 1.5s linear 0.5s, transform 1.5s"
+        e.target.parentNode.style.transform = "rotateY(-180deg)"
+        const index = parseInt(e.target.parentNode.style.zIndex) * -1
+        e.target.parentNode.style.zIndex = `${index}`
+      } else {
+        e.target.parentNode.style.transition = "z-index 0.5s, transform 1.5s"
+        // e.target.parentNode.parentNode.style.transition = "z-index 1s, transform 1.5s"
+        e.target.parentNode.style.transform = ""
+        const index = parseInt(e.target.parentNode.style.zIndex) * -1
+        e.target.parentNode.style.zIndex = `${index}`
+      }
+    }  
+
+  return (
+    <div className="book__page book__page--innerpages" style={{zIndex: `${zIndex}`}}>
+      <div className="book__page-front" onClick={pageFlip}>
+        <h1>THIS HAS ITEM:</h1>
+        <p>{a}, {b}, {c}, {d}</p>
+        <div className="page__number">{page}</div>
+      </div>
+      <div className="book__page-back" onClick={pageFlip}>
+        <h1>THIS HAS ITEM:</h1>
+        <p>{e}, {f}, {g}, {h}</p>
+        <div className="page__number">{page}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Pages;
