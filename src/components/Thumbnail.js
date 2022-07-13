62121b2d9f5c81f5252c31cf8aec018a91c0583b
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-07-13 10:59:15 -0400

    fixed search name

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -4,7 +4,9 @@ import { NavLink } from 'react-router-dom';
 function Thumbnail({item, setSelectedItem}) {
   const {id, attributes} = item
 
-  const itemTitle = attributes.titles.en_us ? attributes.titles.en_us : (attributes.titles.en ? attributes.titles.en : attributes.titles.en_jp)
+  // const itemTitle = attributes.titles.en_us ? attributes.titles.en_us : (attributes.titles.en ? attributes.titles.en : attributes.titles.en_jp)
+
+  const itemTitle = attributes.canonicalTitle
 
   return (
     <div className="item">
