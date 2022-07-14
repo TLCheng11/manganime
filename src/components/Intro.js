964f7f80f0524c162294aa11445235da2bbfd67d
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-07-14 17:44:44 -0400

    added skip intro function

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -46,6 +46,8 @@ function Intro({manga, anime, firstStart, setfirstStart}) {
   const introTitleRef = useRef()
   const introTitle = introTitleRef.current
 
+  const timerIds = useRef([])
+
   //loop random image
   useEffect(() => {
     intervalId = setInterval(() => {
@@ -63,97 +65,116 @@ function Intro({manga, anime, firstStart, setfirstStart}) {
   }, [])
 
   function enterWebpage() {
-    // body = document.body
-    // body.style.overflow = "auto"
     musicElementRef.current.play()
     startIntroVideo()
+    window.scrollTo(0, 0)
+    introTitle.style.display = "none"
+    timerIds.current.push(setTimeout(() => {
+      window.scrollTo(0, 0)
+      intro.style.opacity = "0"
+      intro.style.zIndex = "-200"
+    }, 28000))
+    timerIds.current.push(setTimeout(() => {
+      intro.style.pointerEvents = "none"
+      intro.style.transition = "opacity 1s"
+      // clearInterval(intervalId)
+      intro.style.opacity = "0.5"
+      setfirstStart(false)
+    }, 29500))
+  }
+
+  function skipIntro() {
+    console.log(timerIds)
+    timerIds.current.forEach(id => {
+      clearInterval(id)
+    })
     introTitle.style.display = "none"
     window.scrollTo(0, 0)
     setTimeout(() => {
       intro.style.opacity = "0"
       intro.style.zIndex = "-200"
-    }, 28000)
+    }, 500)
     setTimeout(() => {
       intro.style.pointerEvents = "none"
       intro.style.transition = "opacity 1s"
       // clearInterval(intervalId)
       intro.style.opacity = "0.5"
       setfirstStart(false)
-    }, 30000)
+    }, 1500)
   }
 
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     startIntroVideo()
-  //   })
-  // }, [])
-
-  //use effect for intro video
+  //function to start intro video
   function startIntroVideo() {
     videobgRef.current.style.zIndex = "500"
-    setTimeout(() => {
+    timerIds.current.push(setTimeout(() => {
       gif1Ref.current.style.opacity = "1"
       // console.log("1s")
-    }, 1000)
-    setTimeout(() => {
+    }, 1000))
+    timerIds.current.push(setTimeout(() => {
       gif1Ref.current.style.opacity = "0"
       // console.log("3.5s")
-    }, 5500)
-    setTimeout(() => {
+    }, 5500))
+    timerIds.current.push(setTimeout(() => {
       gif2Ref.current.style.opacity = "1"
-    }, 5000)
-    setTimeout(() => {
+    }, 5000))
+    timerIds.current.push(setTimeout(() => {
       gif2Ref.current.style.opacity = "0"
-    }, 10500)
-    setTimeout(() => {
+    }, 10500))
+    timerIds.current.push(setTimeout(() => {
       gif3Ref.current.style.opacity = "1"
-    }, 10000)
-    setTimeout(() => {
+    }, 10000))
+    timerIds.current.push(setTimeout(() => {
       gif3Ref.current.style.opacity = "0"
-    }, 17500)
-    setTimeout(() => {
+    }, 17500))
+    timerIds.current.push(setTimeout(() => {
       gif4Ref.current.style.opacity = "1"
-    }, 17000)
-    setTimeout(() => {
+    }, 17000))
+    timerIds.current.push(setTimeout(() => {
       gif4Ref.current.style.opacity = "0"
-    }, 20500)
-    setTimeout(() => {
+    }, 20500))
+    timerIds.current.push(setTimeout(() => {
       gif5Ref.current.style.opacity = "1"
-    }, 20000)
-    setTimeout(() => {
+    }, 20000))
+    timerIds.current.push(setTimeout(() => {
       gif5Ref.current.style.opacity = "0"
-    }, 23500)
-    setTimeout(() => {
+    }, 23500))
+    timerIds.current.push(setTimeout(() => {
       gif6Ref.current.style.opacity = "1"
-    }, 23000)
-    setTimeout(() => {
+    }, 23000))
+    timerIds.current.push(setTimeout(() => {
       gif6Ref.current.style.opacity = "0"
-    }, 24000)
-    setTimeout(() => {
+    }, 24000))
+    timerIds.current.push(setTimeout(() => {
       gif7Ref.current.style.opacity = "1"
-    }, 24000)
-    setTimeout(() => {
+    }, 24000))
+    timerIds.current.push(setTimeout(() => {
       gif7Ref.current.style.opacity = "0"
-    }, 26000)
-    setTimeout(() => {
+    }, 26000))
+    timerIds.current.push(setTimeout(() => {
       videobgRef.current.style.opacity = "0"
-    }, 28400)
-    setTimeout(() => {
+    }, 28400))
+    timerIds.current.push(setTimeout(() => {
       videobgRef.current.style.zIndex = "-500"
-    }, 32000)
+    }, 32000))
   }
 
   return (
     <div id="intro" ref={introRef}>
-      <div id="video-background" ref={videobgRef} >
-        <img id="gif-1" src={gif1} ref={gif1Ref} />
-        <img id="gif-2" src={gif2} ref={gif2Ref} />
-        <img id="gif-3" src={gif3} ref={gif3Ref} />
-        <img id="gif-4" src={gif4} ref={gif4Ref} />
-        <img id="gif-5" src={gif5} ref={gif5Ref} />
-        <img id="gif-6" src={gif6} ref={gif6Ref} />
-        <img id="gif-7" src={gif7} ref={gif7Ref} />
-      </div>
+      {
+        firstStart ? (
+        <div id="video-background" ref={videobgRef} onClick={skipIntro} >
+          <img id="gif-1" src={gif1} ref={gif1Ref} />
+          <img id="gif-2" src={gif2} ref={gif2Ref} />
+          <img id="gif-3" src={gif3} ref={gif3Ref} />
+          <img id="gif-4" src={gif4} ref={gif4Ref} />
+          <img id="gif-5" src={gif5} ref={gif5Ref} />
+          <img id="gif-6" src={gif6} ref={gif6Ref} />
+          <img id="gif-7" src={gif7} ref={gif7Ref} />
+        </div>
+        ) : (
+          null
+        )
+      }
       <audio autoPlay controls loop id="background-music" type="audio/mpeg" src={musicLink} ref={musicElementRef} ></audio>
       <img src={image} />
       <div id="intro-title" onClick={enterWebpage} ref={introTitleRef} >
